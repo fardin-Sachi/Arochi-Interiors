15d2c71d4d4a31a29aa7ce556c531c13e2ed55b9
Fardin Ahsan <fardin2002@cseku.ac.bd>
2025-07-13 02:10:24 +0600

    Modify notFound.js.

--- app/not-found.js
+++ app/not-found.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function NotFound() {
   return (
-    <div className='text-center text-4xl font-[poppins] font-extrabold'>
-        Bruh, this was only a landing page project
+    <div className='text-center text-4xl font-[poppins] font-extrabold absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2'>
+        <h1 className='p-10'>Oops...</h1>
+        <h2>This was only a landing page project.</h2>
     </div>
   )
 }
